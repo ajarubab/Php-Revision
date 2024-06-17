4b89633b44a4d22324486463fd45b77ec7288769
RKP <ajarubab@gmail.com>
2024-06-17 15:01:25 +0530

    use of variables in php and with html tags

---
+++ useOfVariables.php
@@ -0,0 +1,29 @@
+<?php
+    $name = "Rakesh";
+    echo "The name is ".$name.".";
+    
+    $Age = 28;
+    echo "<br>The age of ".$name." is ".$Age.".";
+
+    $digit = 7;
+    echo "<br>The value of 7 multiply 10 is ".$digit*10;
+?>
+
+<h1 style="color:red">
+    <?php echo $Age." / ".$digit." = ".$Age/$digit; ?>
+</h1>
+
+
+<!-- -------------------------------------------------------------------------- -->
+<?php
+$h3_tag_color="Blue";
+?>
+
+<h3 style="color:<?php echo $h3_tag_color?>">This is First line of h3 Tag.</h3>
+<h3 style="color:<?php echo $h3_tag_color?>">This is Second line of h3 Tag.</h3>
+<h3 style="color:<?php echo $h3_tag_color?>">This is Third line of h3 Tag.</h3>
+<h3 style="color:<?php echo $h3_tag_color?>">This is Fourth line of h3 Tag.</h3>
+<h3 style="color:<?php echo $h3_tag_color?>">This is Fifth line of h3 Tag.</h3>
+<h3 style="color:<?php echo $h3_tag_color?>">This is Sixth line of h3 Tag.</h3>
+
+<!---------------------------------------------------------------------------- -->
