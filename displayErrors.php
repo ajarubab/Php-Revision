2682c8f36a644bafeef897c750622963ba2a3af2
RKP <ajarubab@gmail.com>
2024-06-17 16:17:35 +0530

    setting change and visualisation of error of php

---
+++ displayErrors.php
@@ -0,0 +1,6 @@
+<?php
+    $empName = "Raja"
+    echo $empName;
+    // go to /Applications/XAMPP/xamppfiles/etc/php.ini then change the display_errors from Off to On 
+    // to see the above error on not using semicolon
+?>
