5ad0751f2ca0a5b09c16b58c6ae1f7423d346266
RKP <ajarubab@gmail.com>
2024-06-17 13:45:12 +0530

    use of echo, print and print() with html and css uses

---
+++ first.php
@@ -0,0 +1,21 @@
+<?php
+    echo "Hello world";
+    echo "</br>Let's Revise Php.";
+    echo "<h1>This is h1 tag inside echo of php tag.</h1>";
+    echo "<h1 style='color:red'>This is h1 tag with some css inside echo of php tag.</h1>";
+?>
+<?php
+    echo("This is echo function.");
+    echo("<h2>This is echo function in h2 size.</h2>");
+    echo("<h2 style='background-color:aqua'>This is echo function in h2 size with some css.</h2>");
+?>
+<?php
+    print "Hello guys";
+    print "<br>How are you?";
+    print "<h1>use of h3 tag within double quotes of print </h3>";
+    print "<h1 style='border:5px solid red'>use of h3 tag within double quotes of print with some css</h3>";
+    print("this is print function of php");
+    print("<br>this is print function of php with br line break html tag");
+    print("<h3>this is print function of php in h3 size</h3>");
+    print("<h4 style='text-decoration: underline 5px red;'>this is print function of php in h4 size with some css</h4>");
+?>
