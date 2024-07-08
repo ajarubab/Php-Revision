5260155af799bca07f7afe87f09d66a2562260b1
RKP <ajarubab@gmail.com>
2024-07-08 13:23:36 +0530

    Constants in php

---
+++ contant.php
@@ -0,0 +1,13 @@
+<?php
+    const name = "Raja kumar prasad";   // way 1 of contant declaration
+    define("NAME","Shree Ram");         // way 2 of contant declaration
+    
+    // we can't use both way for same contant
+    // we cant use either of ways on double same-same declaration
+    // we can't reassign values in both pre-declared constants
+    // name and NAME are different due to casing of letters
+    
+    echo name;
+    echo NAME;
+
+?>
--- displayErrors.php
+++ displayErrors.php
@@ -1,5 +1,5 @@
 <?php
-    $empName = "Raja"
+    $empName = "Raja";
     echo $empName;
     // go to /Applications/XAMPP/xamppfiles/etc/php.ini then change the display_errors from Off to On 
     // to see the above error on not using semicolon
