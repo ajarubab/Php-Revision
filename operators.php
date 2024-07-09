b1e54251861765451fc27859d06ea1beaefe626c
RKP <ajarubab@gmail.com>
2024-07-09 12:59:29 +0530

    Operators of php added

--- operators.php
+++ operators.php
@@ -247,6 +247,68 @@
 
     echo "<br>";
     $fullName.=" Jai Hanuman";
-    echo $fullName
+    echo $fullName;
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+
+    // -------------------- Redeclaring the values ----------------
+
+    $a = 20;
+    $b = 4;
+    $c = 5;
+    
+    echo "<b>After Redeclaring values</b><br>";
+    echo "<b>value of \$a  is : ".$a."</b><br>";
+    echo "<b>value of \$b  is : ".$b."</b><br>";
+    echo "<b>value of \$c  is : ".$c."</b><br>";
+    
+    echo "<br>";
+    
+    // ------------------------------------------------------------
+
+    // logical operators
+
+    if($a <> null && $a > 5) {
+        echo " \$a ki value 5 se jyada hai";
+    }
+    
+    echo "<br>";
+    if($a <> null and $a > 5) {
+        echo " \$a value is greter than 5";
+    }
+    
+    echo "<br>";
+    if($a == 15 || $a > 15) {
+        echo " \$a ki value 15 se jyada hai";
+    }
+    
+    echo "<br>";
+    if($a == 15 or $a > 15) {
+        echo " \$a value is greater than 15";
+    }
+    
+    echo "<br>";
+    if($a == 15 or $a > 15 || $a > 25) {
+        echo "Yes, \$a value is greater than 15";
+    }
+    
+    echo "<br>";
+    if($a != null and $a > 15 && $a < 25) {
+        echo "Bilkul, \$a ki value 15 se jyada hai";
+    }
+    
+    echo "<br>";
+    if($a != null and $a > 15 && ($a < 25 || $a > 10)  ) {
+        echo "Ji, \$a ki value 15 se jyada hai";
+    }
+    
+    echo "<br>";
+    // in xor only one condition should be true, nither both nor none
+    if($a == 10 xor $b == 4) {
+        echo " xor condtion met sussessfully.";
+    } else {
+        echo " xor condtion not met.";
+    }
 
 ?>
