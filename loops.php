6b78f5fc14a5787eb009567172d46f88c3058717
RKP <ajarubab@gmail.com>
2024-07-09 13:48:50 +0530

    added for, nested-for, do-while loops

--- loops.php
+++ loops.php
@@ -8,5 +8,38 @@ while($a<=10){
     $a+=1;
 }
 
-//
+echo "<br>";
+echo "<br>";
+
+// for loop
+
+for($i = 1; $i <= 10 ; $i++){
+    echo 5*$i." ";
+}
+
+echo "<br>";
+echo "<br>";
+
+// nested for loop
+
+for($i = 1; $i <= 20 ; $i++){
+    for($j = 1; $j <= 10 ; $j++){
+        echo $i*$j." ";
+    }
+    echo "<br>";
+}
+
+
+echo "<br>";
+echo "<br>";
+
+// do-while loop
+
+$b = 1;
+do{
+    echo 15*$b." ";
+    $b+=1;
+}while($b<11);
+
+
 ?>
