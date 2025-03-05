a075a1efce99488ecc6f9a794fbc8d2f7789bfb9
Raja kumar prasad <ajarubab@gmail.com>
2025-03-05 16:43:26 +0530

    php basic syntax revision

--- first.php
+++ first.php
@@ -29,3 +29,20 @@
 
 
 ?>
+<?php
+// Revision on 05/03/2025
+echo "Hello world<br>";
+echo ("Hello Everyone");
+echo "<h1>Hello All</h1>";
+echo "<h1 style='color:Green'>Har Har Mahadev</h1>";
+echo ("<h1>Radhe Radhe</h1>");
+echo ("<h1 style='color:orange'>Jai siya ram</h1>");
+echo "<h2 style='background-color:Aqua;width:100px;border: 3px solid red;text-align:center'>Om Namah Shivay</h2>";
+echo "<br><br>";
+print "Hi<br>";
+print "<h1>Namaste</h1>";
+print "<h1 style='border: 4px dotted blue; width:fit-content'>Namaste Bharat</h1>";
+print("Hi all");
+print("<h1>This is line under print</h1>");
+print("<h1 style='color:Blue'>This is line under print with some styling</h1>");
+?>
